102f0096c9c13eaf4ae085a48b7385dbd63de64c
Trs-starkiller34 <tristanrobbgin@gmail.com>
2024-09-03 12:34:22 +0700

    perkuliahan 1

---
+++ 1_intro.py
@@ -0,0 +1,24 @@
+print("starkiller34")
+# deklarasi variabel
+a = float(2.5)
+b = 2
+c = a * b 
+d = a / b
+e = a - b
+
+# operasi (operation) : tambah(+), kurang(-), bagi(/), kali(*), modulo(%), pangkat(**)
+print(a)
+print(b)
+print(c) 
+
+perkalian = a * b
+pembagian = a / b
+pengurangan = a - b
+modulo = a % b
+pangkat = a ** b
+
+print(perkalian)
+print(pembagian)
+print(pengurangan)
+print(modulo)
+print(pangkat)
